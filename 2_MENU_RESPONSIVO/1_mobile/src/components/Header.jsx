e23c19fac9159a87e6cb0d8f615646e50d62a055
EA - Henrique Fiorini Golfetto <henrique.golfetto@eaportal.org>
2023-09-14 16:45:11 -0300

    menu

---
+++ 2_MENU_RESPONSIVO/1_mobile/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { List } from "@phosphor-icons/react"
+import { Fire } from "@phosphor-icons/react"
+import Styles from "./Header.module.css"
+function Header() {
+  return (
+    <header className={Styles.header}>
+        <div className={Styles.mobile}>
+            <div className={Styles.menu}>
+                <button>
+                    <List size={32}/>
+                </button>
+
+                <h1><Fire size={32} color="#c22b05" /></h1>
+            </div>
+            <div className="options">
+              <nav>
+                <a href="">Home</a>
+                <a href="">Resultados</a>
+                <a href="">Produtos</a>
+                <a href="">Login</a>
+              </nav>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
