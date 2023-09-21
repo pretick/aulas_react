d681a8d1a785a39eac0a66365bffe1e811b3dc67
EA - Henrique Fiorini Golfetto <henrique.golfetto@eaportal.org>
2023-09-21 14:06:45 -0300

    Menu abrindoo

--- 2_MENU_RESPONSIVO/1_mobile/src/components/Header.jsx
+++ 2_MENU_RESPONSIVO/1_mobile/src/components/Header.jsx
@@ -1,19 +1,36 @@
-import { List } from "@phosphor-icons/react"
+import { List, X } from "@phosphor-icons/react"
+import { useState } from "react"
 import { Fire } from "@phosphor-icons/react"
 import Styles from "./Header.module.css"
 function Header() {
+   const [AbrirMenu, setAbrirMenu] = useState(false)
+
+   function handleAbrirFecharMenu () {
+      if (AbrirMenu === true) {
+        setAbrirMenu(false)
+        return
+      }
+
+      setAbrirMenu(true)
+   }
   return (
     <header className={Styles.header}>
         <div className={Styles.mobile}>
             <div className={Styles.menu}>
-                <button>
-                    <List size={32}/>
+                <button onClick={handleAbrirFecharMenu}>
+                    {
+                      AbrirMenu === true ? (
+                        <X size={32} />
+                      ) : (
+                        <List size={32} />
+                      )
+                    }
                 </button>
 
                 <h1><Fire size={32} color="#c22b05" /></h1>
             </div>
-            <div className={Styles.options}>
-              <nav>
+            <div className={`${Styles.options} ${AbrirMenu === true && Styles.open}`}>
+              <nav className="">
                 <a href="">Home</a>
                 <a href="">Resultados</a>
                 <a href="">Produtos</a>
